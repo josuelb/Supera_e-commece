5711b354d246a778e96a1f9701fa6db6889a9d01
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2023-02-12 23:00:41 -0300

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -1,8 +1,8 @@
 python -m venv ./venv;
 ./venv/Scripts/activate;
-pip install django django-allauth
-pip install djangorestframework djangorestframework_simplejwt[crypto]
+pip install django django-allauth;
+pip install djangorestframework djangorestframework_simplejwt[crypto];
 pip install Pillow psycopg2 psycopg2-binary;
-pip install django-cors-headers
+pip install django-cors-headers;
 cd backend;
 python manage.py runserver;
