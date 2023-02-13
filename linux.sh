b21046040c7b83c176f6ad2cdeb5fd09edcd7e97
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2023-02-12 23:01:29 -0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,5 +1,8 @@
 python3 -m venv ./venv;
 source ./venv/bin/activate;
-pip3 install django django-allauth djangorestframework djangorestframework_simplejwt[crypto] Pillow psycopg2 psycopg2-binary;
+pip3 install django django-allauth;
+pip3 install djangorestframework djangorestframework_simplejwt[crypto];
+pip3 install Pillow psycopg2 psycopg2-binary;
+pip3 install django-cors-headers;
 cd backend;
 python3 manage.py runserver;
